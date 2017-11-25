a47b678f6d46f5af014f26132ab08b9f85c8376b
markus <toomy@bluewin.ch>
2017-11-25 08:07:08 +0100

    Updated to latest simulation base

---
+++ src/main/java/ch/zhaw/sd/delegates/SendMessageByJavaAPI.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.zhaw.sd.delegates;
+
+import ch.zhaw.sd.exceptions.SendMessageException;
+import java.util.HashMap;
+import java.util.Map;
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+
+/**
+ *
+ * @author kell
+ */
+public class SendMessageByJavaAPI implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution de) throws Exception {
+        //Listener expects following local variables:
+        System.out.println("SendMessageByJavaAPI expects following local variables:");
+        System.out.println("messageName: Name of receiving message event");
+        System.out.println("businessKey: businessKey of new instance or waiting instance");
+        System.out.println("messageBody: a Map of variables");
+        System.out.println("throws BpmnError 'SendMessageException'");
+        String messageName, businessKey, correlationKey;
+        Map<String,Object> messageBody;
+        try{
+            if(de.hasVariableLocal("messageName")){
+                messageName = (String) de.getVariableLocal("messageName");
+            } else {
+                messageName="";
+                System.out.println("SendMessageByJavaAPI: Sending message failed due to missing messageName");
+                throw new SendMessageException("Missing local variable 'messageName'");
+            }
+            if(de.hasVariableLocal("businessKey")){
+                businessKey = (String) de.getVariableLocal("businessKey");
+            } else {
+                businessKey = "";
+                System.out.println("SendMessageByJavaAPI: Sending message failed due to missing businessKey");
+                throw new SendMessageException("Missing local variable 'businessKey'");
+            }
+            if(de.hasVariableLocal("correlationKey")){
+                correlationKey = (String) de.getVariableLocal("correlationKey");
+                System.out.println("SendMessageByJavaAPI: correlationKey="+correlationKey);
+            } else {
+                correlationKey = null;
+                System.out.println("SendMessageByJavaAPI: no correlationKey --> starting new instance");
+            }
+            if(de.hasVariableLocal("messageBody")){
+                messageBody = (Map<String, Object>) de.getVariableLocal("messageBody");
+            } else {
+                System.out.println("SendMessageByJavaAPI: Sending message with no messageBody");
+                messageBody = new HashMap<String,Object>();
+            }
+            if(correlationKey==null){
+                //trying to start new instance
+                de.getProcessEngineServices().getRuntimeService()
+                        .startProcessInstanceByMessage(messageName, businessKey, messageBody);
+            } else {
+                //trying to start waiting instance
+                MessageCorrelationResult result =  de.getProcessEngineServices()
+                        .getRuntimeService()
+                        .createMessageCorrelation(messageName)
+                        .processInstanceVariableEquals("correlationKey", correlationKey)
+                        .setVariables(messageBody)
+                        .correlateWithResult();
+                //Execution exec = result.getExecution();
+                //de.getProcessEngineServices().getRuntimeService().messageEventReceived(messageName, exec.getId(), messageBody);
+            }
+        } catch (SendMessageException ex){
+            throw new BpmnError("SendMessageException", ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("SendMessageByJavaAPI: sending message failed");
+            System.out.println(ex.getMessage());
+            throw new BpmnError("SendMessageException", ex.getMessage());
+        }
+    }
+    
+}
---
+++ src/main/java/ch/zhaw/sd/delegates/SendMessageByRestAPI.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.zhaw.sd.delegates;
+
+import java.util.Map;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.connect.Connectors;
+import org.camunda.connect.httpclient.HttpConnector;
+import org.camunda.connect.httpclient.HttpResponse;
+import static org.camunda.spin.Spin.JSON;
+import org.camunda.spin.json.SpinJsonNode;
+
+/**
+ *
+ * @author kell
+ */
+public class SendMessageByRestAPI implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution de) throws Exception {
+        //Reading local variables
+        String messageName, businessKey, url, method;
+        if (de.hasVariableLocal("messageName")){
+            messageName = (String) de.getVariableLocal("messageName");
+        } else {
+            messageName = "";
+        }
+        System.out.println("messageName: "+messageName);
+        if (de.hasVariableLocal("businessKey")){
+            businessKey = (String) de.getVariableLocal("businessKey");
+        } else {
+            businessKey = de.getBusinessKey();
+        }
+        System.out.println("businessKey: "+businessKey);
+        if (de.hasVariableLocal("url")){
+            url = (String) de.getVariableLocal("url");
+        } else {
+            url = "http://localhost:8080/engine-rest/message";
+        }
+        System.out.println("url: "+url);
+        if (de.hasVariableLocal("method")){
+            method = (String) de.getVariableLocal("method");
+        } else {
+            method = "post";
+        }
+        System.out.println("method: "+method);
+        Map<String,Object> params, corrKeys;
+        if (de.hasVariableLocal("params")){
+            params = (Map<String,Object>) de.getVariableLocal("params");
+        } else {
+            params = null;
+        }
+        if (de.hasVariableLocal("corrKeys")){
+            corrKeys = (Map<String,Object>) de.getVariableLocal("corrKeys");
+        } else {
+            corrKeys = null;
+        }
+        //defaults
+        String headers = "application/json;charset=UTF-8";
+        
+        //build payload - works only for simple types
+        String payload = "{";
+        payload = payload
+                + "\"messageName\" : \""
+                + messageName
+                + "\"";
+        if(businessKey!=null){
+            payload = payload 
+                    + ",\"businessKey\" : \""
+                    + businessKey
+                    + "\"";
+        }
+        
+        if(corrKeys!=null){
+            String correlation = "{";
+            for(Map.Entry<String,Object> entry : corrKeys.entrySet()){
+                correlation = correlation + "\""
+                        + entry.getKey()
+                        + "\" : {\"value\" : \""
+                        + entry.getValue().toString()
+                        + "\", \"type\": \""
+                        + entry.getValue().getClass().getSimpleName()
+                        + "\"},";
+            }
+            correlation = correlation.substring(0, correlation.length()-1);
+            correlation = correlation + "}";
+            payload = payload + ",\"correlationKeys\" : "
+                    + correlation;
+        }
+        
+        if(params!=null){
+            String processVariables = "{";
+            for(Map.Entry<String,Object> entry : params.entrySet()){
+                processVariables = processVariables + "\""
+                        + entry.getKey()
+                        + "\" : {\"value\" : \""
+                        + entry.getValue().toString()
+                        + "\", \"type\": \""
+                        + entry.getValue().getClass().getSimpleName()
+                        + "\"},";
+            }
+            processVariables = processVariables.substring(0, processVariables.length()-1);
+            processVariables = processVariables + "}";
+            payload = payload + ",\"processVariables\" : "
+                    + processVariables;
+        }
+
+        //add "resultEnabled" : true
+        payload = payload + ",\"resultEnabled\" : true";
+        
+        payload = payload + "}";
+        System.out.println("payload: "+payload);
+        
+        //make http call
+        HttpConnector http = Connectors.getConnector(HttpConnector.ID);
+        HttpResponse response = http.createRequest()
+                .method(method)
+                .url(url)
+                .contentType("text/plain")
+                .header("Accept","application/json")
+                .payload(payload)
+                .execute();
+        Integer statusCode = response.getStatusCode();
+        String contentTypeHeader = response.getHeader("Content-Type");
+        String body = response.getResponse();
+        System.out.println("RestClient: " + body);
+        SpinJsonNode json = JSON(body);
+        System.out.println("Response:"+json);
+        response.close();
+    }
+    
+}
